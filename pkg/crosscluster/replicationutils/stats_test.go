DarrylWong/fork

crosscluster: extract replication RangeStats to util package
Previously replication streams used by PCR and LDR contained
range stats for spans being scanned. This was used to construct
metrics that showed progress of a PCR/LDR initial scan or
catchup scan.

We wish to do the same for changefeeds. This change extracts
the relevant RangeStats protobuf and stats poller into a util
package that will be used by changefeeds in the future.

pkg/util/rangescanstats/stats_test.go
@@ -1,9 +1,4 @@
-// Copyright 2024 The Cockroach Authors.
-//
-// Use of this software is governed by the CockroachDB Software License
-// included in the /LICENSE file.
-
-package producer
+package rangescanstats
 
 import (
 	"context"
@@ -11,12 +6,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cockroachdb/cockroach/pkg/repstream/streampb"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/util/hlc"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/rangedesc"
+	"github.com/cockroachdb/cockroach/pkg/util/rangescanstats/rangescanstatspb"
 	"github.com/cockroachdb/cockroach/pkg/util/span"
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
@@ -71,14 +66,14 @@ func (r *rangeIteratorFactory) NewIterator(
 
 func TestNewPoller(t *testing.T) {
 	defer leaktest.AfterTest(t)()
-
+	const laggingSpanThreshold = 2 * time.Minute
 	type testCase struct {
 		name          string
 		ranges        []string
 		tracked       []string
 		completed     []string
 		lagging       []string
-		expectedStats streampb.StreamEvent_RangeStats
+		expectedStats rangescanstatspb.RangeStats
 	}
 	tests := []testCase{
 		{
@@ -95,7 +90,7 @@ func TestNewPoller(t *testing.T) {
 				// note the gap of "d,e"
 				"e,f",
 			},
-			expectedStats: streampb.StreamEvent_RangeStats{
+			expectedStats: rangescanstatspb.RangeStats{
 				RangeCount:         1,
 				ScanningRangeCount: 1,
 			},
@@ -105,7 +100,7 @@ func TestNewPoller(t *testing.T) {
 			ranges:    []string{"a,b", "c,d"},
 			tracked:   []string{"a,b", "c,d"},
 			completed: []string{"a,b"},
-			expectedStats: streampb.StreamEvent_RangeStats{
+			expectedStats: rangescanstatspb.RangeStats{
 				RangeCount:         2,
 				ScanningRangeCount: 1,
 			},
@@ -115,7 +110,7 @@ func TestNewPoller(t *testing.T) {
 			ranges:    []string{"a,c", "d,e"},
 			tracked:   []string{"a,c", "d,e"},
 			completed: []string{"a,b"},
-			expectedStats: streampb.StreamEvent_RangeStats{
+			expectedStats: rangescanstatspb.RangeStats{
 				RangeCount:         2,
 				ScanningRangeCount: 2,
 			},
@@ -127,7 +122,7 @@ func TestNewPoller(t *testing.T) {
 			completed: []string{"a,b"},
 			lagging:   []string{"c,d"},
 
-			expectedStats: streampb.StreamEvent_RangeStats{
+			expectedStats: rangescanstatspb.RangeStats{
 				RangeCount:         3,
 				ScanningRangeCount: 1,
 				LaggingRangeCount:  1,
@@ -158,7 +153,7 @@ func TestNewPoller(t *testing.T) {
 				require.NoError(t, err)
 			}
 
-			poller := startStatsPoller(context.Background(), time.Minute, trackedSpans, frontier, ranges)
+			poller := StartStatsPoller(context.Background(), time.Minute, trackedSpans, frontier, ranges, laggingSpanThreshold)
 			testutils.SucceedsSoon(t, func() error {
 				stats := poller.stats.Load()
 				if stats == nil {

pritesh-lahoti/cockroach

pkg/crosscluster/replicationutils/stats_test.go
@@ -8,8 +8,8 @@ package replicationutils
 import (
 	"testing"
 
-	"github.com/cockroachdb/cockroach/pkg/repstream/streampb"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/rangescanstats/rangescanstatspb"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,22 +18,22 @@ func TestRangeStats(t *testing.T) {
 
 	type testCase struct {
 		name        string
-		stats       streampb.StreamEvent_RangeStats
+		stats       rangescanstatspb.RangeStats
 		fraction    float32
 		expectedMsg string
 
-		inputStats map[int32]*streampb.StreamEvent_RangeStats
+		inputStats map[int32]*rangescanstatspb.RangeStats
 	}
 
 	testCases := []testCase{
 		{
 			name: "steady state",
-			stats: streampb.StreamEvent_RangeStats{
+			stats: rangescanstatspb.RangeStats{
 				RangeCount: 10,
 			},
 			fraction:    1,
 			expectedMsg: "",
-			inputStats: map[int32]*streampb.StreamEvent_RangeStats{
+			inputStats: map[int32]*rangescanstatspb.RangeStats{
 				1: {RangeCount: 5},
 				2: {RangeCount: 3},
 				3: {RangeCount: 2},
@@ -41,14 +41,14 @@ func TestRangeStats(t *testing.T) {
 		},
 		{
 			name: "initial scan",
-			stats: streampb.StreamEvent_RangeStats{
+			stats: rangescanstatspb.RangeStats{
 				RangeCount:         10,
 				ScanningRangeCount: 6,
 				LaggingRangeCount:  2,
 			},
 			fraction:    0.4,
 			expectedMsg: "initial scan on 6 out of 10 ranges",
-			inputStats: map[int32]*streampb.StreamEvent_RangeStats{
+			inputStats: map[int32]*rangescanstatspb.RangeStats{
 				1: {RangeCount: 5, ScanningRangeCount: 4},
 				2: {RangeCount: 3, ScanningRangeCount: 2},
 				3: {RangeCount: 2, ScanningRangeCount: 0, LaggingRangeCount: 2},
@@ -56,13 +56,13 @@ func TestRangeStats(t *testing.T) {
 		},
 		{
 			name: "lagging",
-			stats: streampb.StreamEvent_RangeStats{
+			stats: rangescanstatspb.RangeStats{
 				RangeCount:        10,
 				LaggingRangeCount: 2,
 			},
 			fraction:    0.8,
 			expectedMsg: "catching up on 2 out of 10 ranges",
-			inputStats: map[int32]*streampb.StreamEvent_RangeStats{
+			inputStats: map[int32]*rangescanstatspb.RangeStats{
 				1: {RangeCount: 5},
 				2: {RangeCount: 3},
 				3: {RangeCount: 2, LaggingRangeCount: 2},
@@ -87,7 +87,7 @@ func TestRangeStats(t *testing.T) {
 				r.Add(id, stats)
 			}
 			total, fraction, msg = rInitializing.RollupStats()
-			require.Equal(t, total, streampb.StreamEvent_RangeStats{})
+			require.Equal(t, total, rangescanstatspb.RangeStats{})
 			require.Equal(t, float32(0), fraction)
 			require.Contains(t, msg, "starting streams")
 		})